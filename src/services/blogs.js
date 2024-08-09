c500eb9f6a22477134a5e707e094efab5f3b4024
Raza <bilgramiraza@yahoo.com>
2024-08-10 01:12:07 +0530

    setup Login Route

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -2,11 +2,17 @@ import axios from 'axios';
 
 const baseUrl = '/api/blogs';
 
+const login = async credentials => {
+  const response = await axios.post(baseUrl, credentials);
+  return response.data;
+};
+
 const getAll = async () => {
   const response = await axios.get(baseUrl);
   return response.data;
 };
 
 export default {
-  getAll
+  login,
+  getAll,
 };
