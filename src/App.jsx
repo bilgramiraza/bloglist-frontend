f1d1ebb3a153f32b0e1081bb948355b872726d38
Raza <bilgramiraza@yahoo.com>
2024-08-10 01:11:27 +0530

    Added Conditional Rendering for Login/Blog List

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
 import { useState, useEffect } from 'react';
+import { getAll, login } from './services/blogs';
 import Blog from './components/Blog';
-import blogService from './services/blogs';
+import LoginForm from './components/LoginForm';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    blogService
-      .getAll()
+    getAll()
       .then(blogs =>
         setBlogs(blogs)
       );
   }, []);
 
+  const handleLogin = async ({ username, password }) => {
+    const credentials = await login({ username, password });
+    setUser(credentials);
+  };
+
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {
+        user === null
+          ? <LoginForm handleLogin={handleLogin} />
+          : blogs.map(blog => <Blog key={blog.id} blog={blog} />)
+      }
     </div>
   );
 };
