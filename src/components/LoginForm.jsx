d6203f46b3ac8920421995b3ff4554b44b28b242
Raza <bilgramiraza@yahoo.com>
2024-08-10 01:08:50 +0530

    Setup Login Form

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+const LoginForm = ({ handleLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (e) => setUsername(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleLogin({ username, password });
+    setUsername('');
+    setPassword('');
+  };
+
+  return (
+    <div>
+      <h2>Login to Application</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" name="username" value={username} onChange={handleUsernameChange} />
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password" value={password} onChange={handlePasswordChange} />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
