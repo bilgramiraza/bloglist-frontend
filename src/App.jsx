348dacd890f02c6360ebb275f612d61c14477686
Raza <bilgramiraza@yahoo.com>
2024-08-12 15:52:24 +0530

    Implemented Notification System and Saving User Tokens to Local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import { useState, useEffect } from 'react';
 import { getAll } from './services/blogs';
 import { login } from './services/login';
-import Blog from './components/Blog';
+import BlogList from './components/Blog';
 import LoginForm from './components/LoginForm';
-
-const BlogList = ({ username, blogs }) => {
-  return (
-    <div>
-      <p>{username} Logged In</p>
-      {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
-    </div>
-  );
-};
+import Notification from './components/Notification';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [user, setUser] = useState(null);
+  const [notification, setNotification] = useState({
+    message: null,
+    status: false,
+  });
 
   useEffect(() => {
     getAll()
@@ -24,14 +20,40 @@ const App = () => {
       );
   }, []);
 
-  const handleLogin = async ({ username, password }) => {
-    const credentials = await login({ username, password });
-    setUser(credentials);
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedInBlogUser');
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      handleNotification(`${user.name} Has Logged In`, true);
+    }
+  }, []);
+
+  const handleNotification = (message, status) => {
+    setNotification({ message, status });
+    setTimeout(() => setNotification(notif => ({ ...notif, message: '' })), 5000);
   };
+
+  const handleLogin = async ({ username, password }) => {
+    try {
+      const credentials = await login({ username, password });
+      setUser(credentials);
+      window.localStorage.setItem('loggedInBlogUser', JSON.stringify(credentials));
+      handleNotification(`${credentials.name} Has Logged In`, true);
+    } catch (err) {
+      handleNotification(err.message, false);
+    }
+  };
+
+  // const handleLogout = async (e) => {
+  //   e.preventDefault();
+  //   window.localStorage.removeItem('loggedInBlogUser');
+  // };
 
   return (
     <div>
       <h2>blogs</h2>
+      <Notification message={notification.message} status={notification.status} />
       {
         user === null
           ? <LoginForm handleLogin={handleLogin} />
