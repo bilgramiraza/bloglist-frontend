cc4694511404bbf9edf4066169fca70608e5c47d
Raza <bilgramiraza@yahoo.com>
2024-08-12 16:54:12 +0530

    Created Component for Creating New Blogs

---
+++ src/components/BlogForm.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+const BlogForm = ({ handleCreation }) => {
+  const [url, setUrl] = useState('');
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleAuthorChange = (e) => setAuthor(e.target.value);
+  const handleTitleChange = (e) => setTitle(e.target.value);
+  const handleUrlChange = (e) => setUrl(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleCreation({ title, author, url });
+    setTitle('');
+    setAuthor('');
+    setUrl('');
+  };
+
+  return (
+    <div>
+      <h2>New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Title:
+          <input type="text" name="title" value={title} onChange={handleTitleChange} />
+        </label>
+        <label>
+          Author:
+          <input type="text" name="author" value={author} onChange={handleAuthorChange} />
+        </label>
+        <label>
+          Url:
+          <input type="text" name="url" value={url} onChange={handleUrlChange} />
+        </label>
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  );
+};
+
+export default BlogForm;
