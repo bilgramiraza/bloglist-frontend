7fe645e08bf87d43859bc462c8440a78fa337d48
Raza <bilgramiraza@yahoo.com>
2024-08-21 21:50:28 +0530

    Added Test for checking if proper items are visible at initial render

---
+++ src/components/Blog.test.jsx
@@ -0,0 +1,45 @@
+import { beforeEach, describe, expect, test, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import Blog from './Blog';
+import userEvent from '@testing-library/user-event';
+
+describe('<Blog />', () => {
+  let container;
+  const testBlog = {
+    title: 'test',
+    author: 'tester',
+    url: 'test.com',
+    likes: '69',
+    user: {
+      username: 'tester',
+    }
+  };
+  const testUser = {
+    username: 'tester',
+  };
+
+  const mockHandler = vi.fn();
+
+  beforeEach(() => {
+    container = render(<Blog
+      blog={testBlog}
+      handleLikes={mockHandler}
+      handleDelete={mockHandler}
+      currentUser={testUser}
+    />).container;
+
+  });
+  test('Renders only Blog Title and Author on initial Render', async () => {
+    const titleDiv = await screen.findByText('test');
+    const authorDiv = await screen.findByText('-tester');
+
+    const urlDiv = await screen.findByText('test.com');
+    const likesDiv = await screen.findByText('69');
+
+    expect(titleDiv).toBeVisible();
+    expect(authorDiv).toBeVisible();
+
+    expect(urlDiv).not.toBeVisible();
+    expect(likesDiv).not.toBeVisible();
+  });
+});
