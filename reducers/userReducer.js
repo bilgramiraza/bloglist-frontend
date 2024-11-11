4fafde075749856cd302364e8bd4c4fa9dfe2be3
Raza Hassan <bilgramiraza@yahoo.com>
2024-11-11 06:51:27 +0000

    Setup Reducer Slice for storing User Details

---
+++ reducers/userReducer.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  name: null,
+  username: null,
+  token: null,
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {},
+});
+
+export const { } = userSlice.actions;
+
+export default userSlice.reducer;
