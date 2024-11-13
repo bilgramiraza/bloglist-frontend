963ab6e6ed891d8709801d78afc3d84483ee281f
Raza Hassan <bilgramiraza@yahoo.com>
2024-11-13 03:37:28 +0000

    Setup Action Reducer for Setting and Clearing User Details

--- reducers/userReducer.js
+++ reducers/userReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { login } from '../src/services/login';
 
 const initialState = {
   name: null,
@@ -10,10 +11,28 @@ const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-
+    setUser(_state, action) {
+      return {
+        name: action.payload.name,
+        username: action.payload.username,
+        token: `Bearer ${action.payload.token}`,
+      };
+    },
+    clearUser() {
+      return initialState;
+    },
   },
 });
 
-export const { } = userSlice.actions;
+export const { setUser, clearUser } = userSlice.actions;
 
 export default userSlice.reducer;
+
+export const loginUser = (username, password) => async dispatch => {
+  try {
+    const credentials = await login({ username, password });
+    dispatch(setUser(credentials));
+  } catch (err) {
+    throw new Error('Failed to Login User');
+  }
+};
