2dc880b1a06a544f52862f2481b412f50b5c5db6
Raza Hassan <bilgramiraza@yahoo.com>
2024-11-20 10:27:55 +0000

    Added Context for Notification

--- src/reducers/notificationReducer.js
+++ src/reducers/notificationReducer.js
@@ -1,3 +1,5 @@
+import { createContext, useReducer, useContext } from 'react';
+
 const initialState = {
   message: null,
   status: false
@@ -15,4 +17,24 @@ const notificationReducer = (state, action) => {
     default:
       return state;
   }
+};
+
+const NotificationContext = createContext();
+
+export const NotificationContextProvider = (props) => {
+  const [notification, dispatch] = useReducer(notificationReducer, initialState);
+
+  <NotificationContext.Provider value={[notification, dispatch]}>
+    {props.children}
+  </NotificationContext.Provider>;
+};
+
+export const useNotificationValue = () => {
+  const notificationAndDispatch = useContext(NotificationContext);
+  return notificationAndDispatch[0];
+};
+
+export const useNotificationDispatch = () => {
+  const notificationAndDispatch = useContext(NotificationContext);
+  return notificationAndDispatch[1];
 };
