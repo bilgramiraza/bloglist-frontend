cd57a36bb08b4f4d50699abfbbb158f918ca3c1b
Raza Hassan <bilgramiraza@yahoo.com>
2024-12-19 22:03:26 +0000

    Setup Slice for Users

--- reducers/store.js
+++ reducers/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import notificationReducer from './notificationReducer';
 import blogsReducer from './blogsReducer';
 import authReducer from './authReducer';
+import usersReducer from './usersReducer';
 
 export const store = configureStore({
   reducer: {
     notification: notificationReducer,
     blogs: blogsReducer,
+    users: usersReducer,
     auth: authReducer
   }
 });
