2ebef2e8d0df85bb979eeb48998ebab7a030c09d
Raza Hassan <bilgramiraza@yahoo.com>
2025-01-01 15:02:11 +0000

    Created Component for Displaying All Blogs by a Specific User

---
+++ src/components/UserBlogList.jsx
@@ -0,0 +1,29 @@
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectBlogsById } from "../../reducers/usersReducer";
+
+function UserBlogList() {
+  const id = useParams().id;
+
+  const userBlogs = useSelector(state => selectBlogsById(state, id));
+
+  if (!userBlogs)
+    return null;
+
+  const userBlogsList = userBlogs.blogs.map(blog =>
+    <li key={blog.id}>
+      {blog.title}
+    </li>
+  );
+
+  return (
+    <div>
+      <h4>{userBlogs.name}</h4>
+      <ul>
+        {userBlogsList}
+      </ul>
+    </div>
+  );
+}
+
+export default UserBlogList;
