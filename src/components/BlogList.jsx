1eb269d23bb39caab53fc97946f4e3e64235d086
Raza Hassan <bilgramiraza@yahoo.com>
2025-01-03 11:57:05 +0000

    Simplified BlogList to only show all blogs As links

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,23 +1,45 @@
-import Blog from './Blog';
 import { useSelector } from 'react-redux';
 import { selectSortedBlogs } from '../../reducers/blogsReducer';
+import { Link } from 'react-router-dom';
 
 const BlogList = () => {
   const blogs = useSelector(selectSortedBlogs);
-  let listOfBlogs;
+
+  const blogStyle = {
+    width: '75%',
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: 2
+  };
+
+  const blogHeaderStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 2
+  };
+
   if (!blogs || !blogs.length) {
-    listOfBlogs = null;
-  } else {
-    listOfBlogs = blogs.map((blog) => (
-      <Blog
-        key={blog._id}
-        blog={blog}
-      />
-    ));
+    return (
+      <div data-testid="bloglist" style={blogStyle}>
+        <p>No Blogs to Display</p>
+      </div>
+    );
   }
+
   return (
-    <div data-testid="bloglist">
-      {listOfBlogs}
+    <div data-testid="bloglist" style={blogStyle}>
+      {blogs.map((blog) => (
+        <div style={blogHeaderStyle} key={blog._id}>
+          <h4>
+            <Link to={`/blogs/${blog._id}`}>{blog.title}</Link>
+          </h4>
+          <p>{`-${blog.author}`}</p>
+        </div>
+      ))}
     </div>
   );
 };
