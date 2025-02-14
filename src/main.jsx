28213d2c0bac50e3733ea88a6103b2fddefb1c3d
Raza Hassan <bilgramiraza@yahoo.com>
2025-02-14 20:54:16 +0000

    Added Dev-Dependency:ReactQueryDevtools

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthContextProvider } from './reducers/authReducer';
 import { UserContextProvider } from './reducers/userReducer';
 import { BrowserRouter } from 'react-router-dom';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -19,5 +20,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </UserContextProvider>
       </AuthContextProvider>
     </NotificationContextProvider>
+    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
 );
