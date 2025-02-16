f57088dea2ee96ccefe416c6bdc4392ba2889d0b
Raza Hassan <bilgramiraza@yahoo.com>
2025-02-16 01:18:19 +0000

    Restructured Logic of Component to make it More Resilent to edge cases

--- src/components/UserSummary.jsx
+++ src/components/UserSummary.jsx
@@ -2,11 +2,11 @@ import { useParams } from "react-router-dom";
 import { notify, useNotificationDispatch } from "../reducers/notificationReducer";
 import { useQuery } from "@tanstack/react-query";
 import { getAll } from "../services/users";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const UserSummary = () => {
   const id = useParams().id;
-  let blogsList = null;
+  const [prevError, setPrevError] = useState(null);
 
   const dispatch = useNotificationDispatch();
 
@@ -14,39 +14,50 @@ const UserSummary = () => {
     queryKey: ['users'],
     queryFn: getAll,
     select: (users) => {
-      const target = users.filter((user) => user.id === id);
-      return target[0];
+      const target = users.find((user) => user.id === id);
+      if (!target)
+        throw new Error('User Not Found');
+      return target;
     },
     retry: false,
-    throwOnError: false,
     staleTime: 60 * 1000,
   });
 
   useEffect(() => {
-    if (userQuery.isError) {
+    if (userQuery.isError && userQuery.error?.message !== prevError) {
       notify(dispatch, userQuery.error.message || 'An Error Occured', false, 5);
+      setPrevError(userQuery.error.message);
     }
-  }, [dispatch, notify, userQuery.error]);
+  }, [dispatch, userQuery.isError, userQuery.error?.message, prevError]);
 
   if (userQuery.isLoading) {
-    blogsList = <li>Loading Blogs</li>;
+    return (
+      <div>
+        <h4>Loading Blogs</h4>
+      </div>
+    );
   }
   if (userQuery.isError) {
-    blogsList = <li>Error Getting Blogs</li>;
-  }
-  if (userQuery.isSuccess) {
-    blogsList = userQuery.data === null
-      ? (<li></li>)
-      : userQuery.data?.blogs.map(blog => (
-        <li key={blog.id}>{blog.title}</li>
-      ));
+    return (
+      <div>
+        <h4>User Not Found</h4>
+      </div>
+    );
   }
 
   return (
     <div>
       <h4>{userQuery.data?.name.toUpperCase()} Blogs</h4>
       <ul>
-        {blogsList}
+        {
+          userQuery.data?.blogs?.length
+            ? userQuery.data?.blogs?.map(blog => (
+              <li key={blog.id}>{blog.title}</li>
+            ))
+            : (
+              <li>No Blogs Found</li>
+            )
+        }
       </ul>
     </div>
   );
