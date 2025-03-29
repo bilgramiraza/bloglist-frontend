13835b732963936d0180f152aab66fa2d9b34120
Raza Hassan <bilgramiraza@yahoo.com>
2025-03-29 08:41:16 +0000

    Removed Unused Reducer

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import App from './App';
 import { NotificationContextProvider } from './reducers/notificationReducer';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthContextProvider } from './reducers/authReducer';
-import { UserContextProvider } from './reducers/userReducer';
 import { BrowserRouter } from 'react-router-dom';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
@@ -13,11 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
     <NotificationContextProvider>
       <AuthContextProvider>
-        <UserContextProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </UserContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </AuthContextProvider>
     </NotificationContextProvider>
     <ReactQueryDevtools initialIsOpen={false} />
