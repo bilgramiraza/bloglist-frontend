90ba3f055ff43f237f68c32df591663ff9ccb17c
Raza Hassan <bilgramiraza@yahoo.com>
2025-04-22 05:58:02 +0000

    Removed Redundant/Outdated Code

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,22 +1,8 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { notify } from "../../reducers/notificationReducer";
-import { initializeUsers } from "../../reducers/usersReducer";
 import { Routes, Route } from 'react-router-dom';
 import UsersSummary from "./UsersSummary";
 import UserBlogList from "./UserBlogList";
 
 function Users() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    try {
-      dispatch(initializeUsers());
-    } catch (err) {
-      dispatch(notify(err.message || 'An Error Occured', false, 3));
-    }
-  }, []);
-
   return (
     <div>
       <h3>Users</h3>
