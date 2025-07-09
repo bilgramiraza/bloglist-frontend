f689632e5b6d1aef940d72d0899c0bb4b2f78b9a
Raza Hassan <bilgramiraza@yahoo.com>
2025-07-09 18:33:25 +0530

    Upgraded Users Slice using Async Thunks

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,32 +1,52 @@
 import { getAll } from '../services/users';
-import { createSelector, createSlice } from '@reduxjs/toolkit';
-
-const initialState = [];
+import { createAsyncThunk, createSelector, createSlice } from '@reduxjs/toolkit';
+import { STATUS } from '../utils/constants';
 
 const usersSlice = createSlice({
   name: 'users',
-  initialState,
-  reducers: {
-    setUsers(_state, action) {
-      return action.payload;
-    }
+  initialState: {
+    items: [],
+    status: {
+      fetch: STATUS.INITIAL
+    },
+    error: null
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUsers.pending, (state) => {
+        state.status.fetch = STATUS.LOADING;
+        state.error = null;
+      })
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.status.fetch = STATUS.SUCCEEDED;
+        state.items = action.payload;
+        state.status.fetch = STATUS.IDLE;
+      })
+      .addCase(fetchUsers.rejected, (state, action) => {
+        state.status.fetch = STATUS.FAILED;
+        state.error = action.payload || action.error.message;
+        state.status.fetch = STATUS.IDLE;
+      });
   }
 });
 
-export const { setUsers } = usersSlice.actions;
-
 export default usersSlice.reducer;
 
-export const selectBlogsByUserId = createSelector(
+export const selectUserById = createSelector(
   [(state) => state.users, (_state, userId) => userId],
-  (users, userId) => users.find((user) => user.id === userId)
+  ({ status, error, items }, userId) => ({
+    status,
+    error,
+    user: items.find((user) => user.id === userId)
+  })
 );
 
-export const initializeUsers = () => async (dispatch) => {
+export const fetchUsers = createAsyncThunk('users/fetchAll', async (_, { rejectWithValue }) => {
   try {
     const users = await getAll();
-    dispatch(setUsers(users));
+    return users;
   } catch (err) {
-    throw new Error(err.message || 'Failed to Get Users');
+    return rejectWithValue(err.message || 'Failed to Get Users');
   }
-};
+});
