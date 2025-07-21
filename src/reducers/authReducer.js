befdc0d732ff3060ad880171d20f8cdf9acc30b5
Raza Hassan <bilgramiraza@yahoo.com>
2025-07-22 03:50:39 +0530

    Setup Async Thunk for Restoring User Login from LocalStorage

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -16,18 +16,11 @@ const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    setUser(state, action) {
-      return {
-        ...state.auth,
-        credentials: {
-          name: action.payload.name,
-          username: action.payload.username,
-          token: `Bearer ${action.payload.token}`
-        }
-      };
-    },
     clearUser() {
-      return initialState;
+      return {
+        ...initialState,
+        status: STATUS.IDLE
+      };
     },
     resetStatus(state) {
       state.status = STATUS.IDLE;
@@ -49,11 +42,25 @@ const authSlice = createSlice({
       .addCase(loginUser.rejected, (state, action) => {
         state.status = STATUS.FAILED;
         state.error = action.payload || action.error.message;
+      })
+      .addCase(restoreUser.pending, (state) => {
+        state.status = STATUS.LOADING;
+        state.error = null;
+      })
+      .addCase(restoreUser.fulfilled, (state, action) => {
+        state.status = STATUS.SUCCEEDED;
+        state.credentials.name = action.payload.name;
+        state.credentials.username = action.payload.username;
+        state.credentials.token = action.payload.token;
+      })
+      .addCase(restoreUser.rejected, (state, action) => {
+        state.status = STATUS.FAILED;
+        state.error = action.payload || action.error.message;
       });
   }
 });
 
-export const { setUser, clearUser, resetStatus } = authSlice.actions;
+export const { clearUser, resetStatus } = authSlice.actions;
 
 export default authSlice.reducer;
 
@@ -62,6 +69,7 @@ export const loginUser = createAsyncThunk(
   async ({ username, password }, { rejectWithValue }) => {
     try {
       const credentials = await login({ username, password });
+      window.localStorage.setItem('loggedInBlogUser', JSON.stringify(credentials));
       return credentials;
     } catch (err) {
       return rejectWithValue(err.message || 'Failed to Login User');
@@ -69,7 +77,18 @@ export const loginUser = createAsyncThunk(
   }
 );
 
-export const logoutUser = () => async (dispatch) => {
+export const logoutUser = () => (dispatch) => {
   dispatch(clearUser());
   window.localStorage.removeItem('loggedInBlogUser');
 };
+
+export const restoreUser = createAsyncThunk('auth/restoreUser', async (_, { rejectWithValue }) => {
+  try {
+    const loggedUserJSON = window.localStorage.getItem('loggedInBlogUser');
+    if (!loggedUserJSON) throw new Error('');
+    const credentials = JSON.parse(loggedUserJSON);
+    return credentials;
+  } catch (err) {
+    return rejectWithValue(err.message || 'Unable to Restore User');
+  }
+});
