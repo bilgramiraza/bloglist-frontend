75efd61252693693d37040602f483816a7fcf93a
Raza Hassan <bilgramiraza@yahoo.com>
2025-07-22 03:51:06 +0530

    Implemented New Restoring User Login Thunk

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,31 +1,38 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { notify } from "../reducers/notificationReducer";
-import { setUser, logoutUser } from "../reducers/authReducer";
+import { logoutUser, resetStatus, restoreUser } from "../reducers/authReducer";
 import { useLocation, useNavigate } from 'react-router-dom';
+import { STATUS } from '../utils/constants';
 
 function Login() {
-  const { name } = useSelector(state => state?.auth?.credentials || {});
+  const {
+    credentials,
+    status,
+  } = useSelector(state => state.auth);
 
   const location = useLocation();
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedInBlogUser');
-    if (loggedUserJSON) {
-      const credentials = JSON.parse(loggedUserJSON);
-      dispatch(setUser(credentials));
+    if (status === STATUS.INITIAL) {
+      dispatch(restoreUser());
+    }
+    if (status === STATUS.SUCCEEDED && credentials?.name) {
       dispatch(notify(`${credentials.name} Has Logged In`));
     }
-  }, []);
+    if (status === STATUS.SUCCEEDED || status === STATUS.FAILED) {
+      dispatch(resetStatus())
+    }
+  }, [status]);
 
   const handleLogin = (e) => {
     e.preventDefault();
     navigate('/login', { state: { from: location }, replace: true });
   };
 
-  const handleLogout = async (e) => {
+  const handleLogout = (e) => {
     e.preventDefault();
     dispatch(logoutUser());
     dispatch(notify('Log out Successful'));
@@ -36,11 +43,11 @@ function Login() {
 
   return (
     <div>
-      {name === null ? (
+      {credentials?.name === null ? (
         <button onClick={handleLogin}>Login</button>
       ) : (
         <p>
-          {name} Logged In <button onClick={handleLogout}>Logout</button>
+          {credentials?.name} Logged In <button onClick={handleLogout}>Logout</button>
         </p>
       )}
     </div>
