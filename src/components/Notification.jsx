8aa39c4fcd5ee85c5826941cc2de7fc31b7328f9
Raza Hassan <bilgramiraza@yahoo.com>
2025-07-29 20:57:11 +0530

    Setup Custom Hooks for useQuery and useMutation to work with Toasts

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,3 +1,5 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useEffect, useRef } from "react";
 import { toast, Toaster } from "sonner";
 
 function Notification() {
@@ -6,6 +8,7 @@ function Notification() {
       richColors={true}
       visibleToasts={9}
       position="top-right"
+      expand={true}
     />
   );
 }
@@ -13,5 +16,97 @@ function Notification() {
 export const notifySuccess = msg => toast.success(msg, { duration: 3000 });
 
 export const notifyError = msg => toast.error(msg, { duration: 5000 });
+
+const serializeQueryKey = (key) => typeof key === 'string' ? key : JSON.stringify(key);
+
+export const useQueryWithToast = ({
+  queryKey,
+  queryFn,
+  queryOptions = {},
+  toastMsg = {
+    loading: 'Loading...',
+    success: 'Success!',
+    error: 'Error',
+  }
+}) => {
+  const query = useQuery({
+    queryKey,
+    queryFn,
+    retry: false,
+    throwOnError: false,
+    staleTime: 60 * 1000,
+    ...queryOptions,
+  });
+
+  const prevError = useRef(false);
+  const didToast = useRef(false);
+
+  useEffect(() => {
+    if (query.isLoading) {
+      toast.loading(toastMsg.loading, {
+        id: serializeQueryKey(queryKey),
+        duration: Infinity,
+      });
+      didToast.current = false;
+    }
+    if (query.isError && query.error?.message !== prevError.current) {
+      toast.error(typeof toastMsg.error === 'function'
+        ? toastMsg.error(query.error)
+        : query.error?.message || toastMsg.error,
+        {
+          id: serializeQueryKey(queryKey),
+          duration: 5000,
+        });
+      prevError.current = query.error?.message;
+      didToast.current = true;
+    }
+    if (query.isSuccess && !didToast.current) {
+      toast.success(typeof toastMsg.success === 'function'
+        ? toastMsg.success(query.data)
+        : toastMsg.success,
+        {
+          id: serializeQueryKey(queryKey),
+          duration: 3000
+        });
+      didToast.current = true;
+    }
+  }, [query.isLoading, query.isSuccess, query.isError, query.error]);
+
+  useEffect(() => {
+    return () => toast.dismiss(serializeQueryKey(queryKey));
+  }, []);
+
+  return query;
+};
+
+export const useMutationWithToast = ({
+  mutationFn,
+  mutationOptions = {},
+  toastMsg = {
+    loading: 'Loading...',
+    success: 'Success!',
+    error: 'Error',
+  },
+}) => {
+  return useMutation({
+    mutationFn: async (mutationData) => {
+      const mutationPromise = mutationFn(mutationData);
+      toast.promise(
+        mutationPromise,
+        {
+          loading: toastMsg.loading,
+          success: (data) => typeof toastMsg.success === 'function'
+            ? toastMsg.success(data)
+            : toastMsg.success,
+          error: (data) => typeof toastMsg.error === 'function'
+            ? toastMsg.error(data)
+            : error?.message || toastMsg.error,
+        }
+      );
+      return await mutationPromise;
+    },
+    ...mutationOptions,
+  });
+}
 
 export default Notification;
