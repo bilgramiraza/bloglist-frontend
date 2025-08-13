37ba9e022229cb47830c28ea1079def642f49ac7
Raza Hassan <bilgramiraza@yahoo.com>
2025-08-13 13:38:07 +0530

    Setup Actions Creators for various Login States

--- src/reducers/authReducer.jsx
+++ src/reducers/authReducer.jsx
@@ -1,22 +1,48 @@
 import { createContext, useReducer, useContext } from 'react';
 import { login } from '../services/login';
+import { STATUS } from '../utils/constants';
 
 const initialState = {
-  name: null,
-  username: null,
-  token: null,
+  status: STATUS.INITIAL,
+  error: null,
+  credentials: {
+    name: null,
+    username: null,
+    token: null,
+  },
 };
 
 const authReducer = (state, action) => {
   switch (action.type) {
-    case 'login':
+    case 'login_start':
       return {
-        name: action.payload.name,
-        username: action.payload.username,
-        token: `Bearer ${action.payload.token}`,
+        ...state,
+        status: STATUS.LOADING,
+      };
+    case 'login_success':
+      return {
+        ...state,
+        status: STATUS.SUCCEEDED,
+        credentials: {
+          name: action.payload.name,
+          username: action.payload.username,
+          token: `Bearer ${action.payload.token}`,
+        }
+      };
+    case 'login_failed':
+      return {
+        ...state,
+        status: STATUS.FAILED,
+        error: action.payload || action.error.message,
       };
     case 'logout':
       return initialState;
+    case 'resetStatus':
+      return {
+        ...state,
+        status: STATUS.IDLE,
+        error: null,
+      };
     default:
       return state;
   }
@@ -45,25 +71,45 @@ export const useAuthDispatch = () => {
 };
 
 export const loginUser = async (dispatch, username, password) => {
+  loginStart(dispatch);
   try {
     const credentials = await login({ username, password });
     setAuth(dispatch, credentials);
-    return credentials;
+    window.localStorage.setItem('loggedInBlogUser', JSON.stringify(credentials));
   } catch (err) {
-    throw new Error(err.message || 'Failed to Login User');
+    loginFailed(dispatch, err.message || 'Failed to Login User');
   }
+};
+
+export const loginStart = (dispatch) => {
+  dispatch({
+    type: 'login_start',
+  });
 };
 
 export const setAuth = (dispatch, credentials) => {
   dispatch({
-    type: 'login',
+    type: 'login_success',
     payload: credentials,
+  });
+};
+
+export const loginFailed = (dispatch, msg) => {
+  dispatch({
+    type: 'login_failed',
+    payload: msg,
   });
 };
 
 export const clearAuth = dispatch => {
   dispatch({
     type: 'logout',
+  });
+};
+
+export const resetStatus = dispatch => {
+  dispatch({
+    type: 'resetStatus',
   });
 };
 
